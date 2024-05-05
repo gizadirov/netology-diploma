467d54e5dded286cf284f91d44f789be15150a91
Timur Khakimov <timurkin@mail.ru>
2024-05-05 14:27:02 +0500

    remove old folders

--- k8s/deploy/maintenance/components/ingress-nginx/helm/install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  -f values.yaml \
-  --namespace ingress-nginx --create-namespace
